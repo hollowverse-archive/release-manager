9699db1d8cafbe71fd3eee3858900e77de4f9e3f
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-21 13:24:08 +0300

    Skip production middleware if it is a test environment

--- src/testProductionEnvironments/middleware.ts
+++ src/testProductionEnvironments/middleware.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import * as cookieParser from 'cookie-parser';
 
 import { environmentsByUrl } from './environmentsByUrl';
 import { createEnvNameGenerator } from './getEnvName';
@@ -10,29 +9,30 @@ const getEnvName = createEnvNameGenerator(weightsByEnvironment);
 
 const testProductionEnvironments = express();
 
-testProductionEnvironments.use(cookieParser());
-
 testProductionEnvironments.use(async (req, res, next) => {
-  const map = await environmentsByUrl;
-  let envName: string | undefined = req.cookies.env;
-  let envUrl: string | undefined;
-  if (!envName || map.get(envName) === undefined) {
-    envName = getEnvName.next().value;
-  }
+  // Skip if request has already been assigned a test environment
+  if (!(req as ExtendedRequest).envUrl) {
+    const map = await environmentsByUrl;
+    let envName: string | undefined = req.cookies.env;
+    let envUrl: string | undefined;
+    if (!envName || map.get(envName) === undefined) {
+      envName = getEnvName.next().value;
+    }
 
-  // Get the URL from the routing map, falling back to first environment
-  // if the environment is defined but does not have a URL
-  envUrl = map.get(envName);
-  if (!envUrl) {
-    envName = map.keys().next().value;
+    // Get the URL from the routing map, falling back to first environment
+    // if the environment is defined but does not have a URL
     envUrl = map.get(envName);
+    if (!envUrl) {
+      envName = map.keys().next().value;
+      envUrl = map.get(envName);
+    }
+
+    res.cookie('env', envName, {
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+
+    (req as ExtendedRequest).envUrl = envUrl;
   }
-
-  res.cookie('env', envName, {
-    maxAge: 24 * 60 * 60 * 1000,
-  });
-
-  (req as ExtendedRequest).envUrl = envUrl;
 
   next();
 });
