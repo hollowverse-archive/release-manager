168a0b9b6f7a93747ab16e267584620fea5bdf5b
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2017-09-21 13:32:06 +0300

    Wrap prod middleware in try/catch

--- src/testProductionEnvironments/middleware.ts
+++ src/testProductionEnvironments/middleware.ts
@@ -10,31 +10,35 @@ const getEnvName = createEnvNameGenerator(weightsByEnvironment);
 const testProductionEnvironments = express();
 
 testProductionEnvironments.use(async (req, res, next) => {
-  // Skip if request has already been assigned a test environment
-  if (!(req as ExtendedRequest).envUrl) {
-    const map = await environmentsByUrl;
-    let envName: string | undefined = req.cookies.env;
-    let envUrl: string | undefined;
-    if (!envName || map.get(envName) === undefined) {
-      envName = getEnvName.next().value;
-    }
+  try {
+    // Skip if request has already been assigned a test environment
+    if (!(req as ExtendedRequest).envUrl) {
+      const map = await environmentsByUrl;
+      let envName: string | undefined = req.cookies.env;
+      let envUrl: string | undefined;
+      if (!envName || map.get(envName) === undefined) {
+        envName = getEnvName.next().value;
+      }
 
-    // Get the URL from the routing map, falling back to first environment
-    // if the environment is defined but does not have a URL
-    envUrl = map.get(envName);
-    if (!envUrl) {
-      envName = map.keys().next().value;
+      // Get the URL from the routing map, falling back to first environment
+      // if the environment is defined but does not have a URL
       envUrl = map.get(envName);
+      if (!envUrl) {
+        envName = map.keys().next().value;
+        envUrl = map.get(envName);
+      }
+
+      res.cookie('env', envName, {
+        maxAge: 24 * 60 * 60 * 1000,
+      });
+
+      (req as ExtendedRequest).envUrl = envUrl;
     }
 
-    res.cookie('env', envName, {
-      maxAge: 24 * 60 * 60 * 1000,
-    });
-
-    (req as ExtendedRequest).envUrl = envUrl;
+    next();
+  } catch (error) {
+    next(error);
   }
-
-  next();
 });
 
 export { testProductionEnvironments };
